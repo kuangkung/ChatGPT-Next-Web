96b20f4d3d6b00eb226cf10aca9cd4ad30f18c96
纤尘 <xianchen@mogu.com>
2023-03-31 16:14:31 +0800

    msg1

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,10 +17,10 @@ try {
 }
 
 export const metadata = {
-  title: "ChatGPT Next Web",
+  title: "XianChen's AI assistant",
   description: "Your personal ChatGPT Chat Bot.",
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "XianChen's AI assistant",
     statusBarStyle: "black-translucent",
   },
   themeColor: "#fafafa",

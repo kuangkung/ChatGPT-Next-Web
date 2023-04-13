a57da5145fc1888290280dc684c4928d2ad1ee53
qiuye <1464815392@qq.com>
2023-04-13 11:52:15 +0800

    feat: 1.修复无法编译问题

--- app/components/navigation/index.tsx
+++ app/components/navigation/index.tsx
@@ -4,19 +4,19 @@ import styles from "./index.module.scss";
 const Navigation = () => {
   return (
     <div>
-      <div className={styles.search}>
-        <input
-          className={styles.search_input}
-          type="text"
-          placeholder="请搜索"
-        />
-        <div className={styles.search_icon}>
-          <img
-            alt="1"
-            src="https://developer.mozilla.org/favicon-48x48.cbbd161b.png"
-          />
-        </div>
-      </div>
+      {/* <div className={styles.search}> */}
+      {/*   <input */}
+      {/*     className={styles.search_input} */}
+      {/*     type="text" */}
+      {/*     placeholder="请搜索" */}
+      {/*   /> */}
+      {/*   <div className={styles.search_icon}> */}
+      {/*     <img */}
+      {/*       alt="1" */}
+      {/*       src="https://developer.mozilla.org/favicon-48x48.cbbd161b.png" */}
+      {/*     /> */}
+      {/*   </div> */}
+      {/* </div> */}
     </div>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,4 @@
-//import Home from "./home/page"
-//export default function App() {
-//  return (
-//    <>
-//      <Home />
-//    </>
-//  );
-//}
-
-//import Navigation from "@/app/components/navigation";
-//export default function App() {
-//  return (
-//    <>
-//      <Navigation />
-//    </>
-//  );
-//}
-
-import Home from "@/app/components/home/page";
+import Home from "@/app/components/home";
 export default function App() {
   return (
     <>
